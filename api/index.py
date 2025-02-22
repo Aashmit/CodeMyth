3916c55009db92a578e767c69c6319c6bb08199c
Aashmyth <aashmit13@gmail.com>
2025-02-22 15:26:03 +0545

    feat: Added generating docs using Ollama locally

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth import router as auth_router
 from .fetch import router as fetch_router
-
+from .generate import router as generate_router
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
 # Enable CORS for Next.js (http://localhost:3000)
@@ -18,3 +18,4 @@ app.add_middleware(
 # Include auth routes
 app.include_router(auth_router, prefix="/api/py")
 app.include_router(fetch_router, prefix="/api/py")
+app.include_router(generate_router, prefix="/api/py")
