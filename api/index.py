7ea55e1b128c0df2450711200bbdb6c8a6a3a9f4
Aashmyth <aashmit13@gmail.com>
2025-02-23 19:15:55 +0545

    fix: updated cors

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 # Enable CORS for Next.js (http://localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow Next.js frontend
+    allow_origins=["*"],  # Allow Next.js frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
